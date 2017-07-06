443da155569569b469891bfc37035dec0e41e937
Brett Fuller <bfuller123@gmail.com>
2017-07-06 11:44:02 -0500

    added form validation

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # quickCRUDapp
 A quick CRUD app to store personnel in a database.
 
-There is no database for this app so the information added will not be persistent.
+There is no database for this app so the information added is only persistent through local storage currently.
+Test users will be added to show a table to begin.
+To clear your local storage, open console and run localStorage.clear().
--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 // TODO: Make it so won't add user unless phone number is correct format, and zipcode is 5 digits
 
-var users = {
+var testUsers = {
   "Brett-Fuller": {
     first: "Brett",
     last: "Fuller",
@@ -24,39 +24,56 @@ var users = {
   }
 };
 
+var storage = {
+  users: {},
+  load: function() {
+    storage.users = JSON.parse(localStorage.getItem('users'));
+  },
+  update: function(obj) {
+    localStorage.setItem('users', JSON.stringify(obj));
+  }
+}
+
 var userFunctions = {
   loadUsers: function() {
     $('.table-body').empty();
-    var allUsers = Object.keys(users);
+    if(localStorage.getItem('users') === null){
+      storage.update(testUsers);
+    }
+    storage.load();
+    console.log(storage.users)
+    var allUsers = Object.keys(storage.users);
     for (var i = 0; i < allUsers.length; i++) {
       var userItem = $('<tr>');
       userItem.attr('class', 'user-row');
       userItem.attr('data-user', allUsers[i]);
-      userItem.append('<td>'+users[allUsers[i]].first+'</td>');
-      userItem.append('<td>'+users[allUsers[i]].last+'</td>');
-      userItem.append('<td>'+users[allUsers[i]].dob+'</td>');
-      userItem.append('<td>'+users[allUsers[i]].phone+'</td>');
-      userItem.append('<td>'+users[allUsers[i]].zip+'</td>');
+      userItem.append('<td>'+storage.users[allUsers[i]].first+'</td>');
+      userItem.append('<td>'+storage.users[allUsers[i]].last+'</td>');
+      userItem.append('<td>'+storage.users[allUsers[i]].dob+'</td>');
+      userItem.append('<td>'+storage.users[allUsers[i]].phone+'</td>');
+      userItem.append('<td>'+storage.users[allUsers[i]].zip+'</td>');
       userItem.append('<td class="user-buttons"><button class="btn btn-sm btn-warning view-button" data-user="'+allUsers[i]+'">View</button><button class="btn btn-sm btn-danger delete-button" data-user="'+allUsers[i]+'">Delete</button></td>');
       $('.table-body').append(userItem);
     }
   },
   deleteUser: function() {
     var user = $(this).attr('data-user');
-    delete users[user];
+    delete storage.users[user];
+    storage.update(storage.users);
     userFunctions.loadUsers();
   },
   viewUser: function() {
     var user = $(this).attr('data-user');
   },
   addUser: function(fullName, firstName, lastName, dateOfBirth, phoneNumber, zipCode) {
-    users[fullName] = {
+    storage.users[fullName] = {
       first: firstName,
       last: lastName,
       dob: dateOfBirth,
       phone: phoneNumber,
       zip: zipCode
     }
+    storage.update(storage.users);
     userFunctions.loadUsers();
   }
 }
@@ -69,20 +86,7 @@ var formFunctions = {
     $('#zip-code').val("");
     $('#phone-number').val("");
   },
-  validateNumbers: function(arr) {
-    for (var i in arr) {
-      if (isNaN(arr[i])){
-        return console.log('That is not valid!');
-      }
-      console.log('Works!');
-    }
-  },
-  splitValue: function(val, split) {
-    var arrayOfVal = val.split(split);
-    return arrayOfVal;
-  },
-  getUserInfo: function(event) {
-    event.preventDefault();
+  getUserInfo: function() {
     var firstName = $('#first-name').val().trim();
     var lastName = $('#last-name').val().trim();
     var dob = $('#dob').val().trim();
@@ -91,6 +95,41 @@ var formFunctions = {
     var fullName = firstName + '-' + lastName;
     userFunctions.addUser(fullName, firstName, lastName, dob, phoneNumber, zipCode);
     formFunctions.clearForm();
+  },
+  validateForm: function() {
+    jQuery.validator.setDefaults({
+      debug: true,
+      success: 'valid'
+    })
+   $('#add-user-form').validate({
+      rules: {
+        firstname: {
+          required: true,
+          minlength: 1
+        },
+        lastname: {
+          required: true,
+          minlength: 1
+        },
+        phonenumber: {
+          required: true,
+          digits: true,
+          rangelength: [10, 10]
+        },
+        zipcode: {
+          required: true,
+          digits: true,
+          rangelength: [5,5]
+        },
+        dob: {
+          required: true
+        }
+      }
+    });
+    console.log($('#add-user-form').valid());
+    if($('#add-user-form').valid()) {
+      formFunctions.getUserInfo();
+    }
   }
 }
 
@@ -98,5 +137,5 @@ $(document).ready(function() {
   userFunctions.loadUsers();
   $('body').on('click', '.delete-button', userFunctions.deleteUser);
   $('body').on('click', '.view-button', userFunctions.viewUser);
-  $('body').on('click', '#add-button', formFunctions.getUserInfo);
+  $('body').on('click', '#add-button', formFunctions.validateForm);
 })
